d020eb40904e85e97146cdce799bcb3dd60fd2bf
dor achinakar <dor259dor@gmail.com>
2022-08-16 13:15:57 +0300

    header and footer 	new file:   .gitignore 	modified:   tpl/header.php

--- tpl/header.php
+++ tpl/header.php
@@ -1,6 +1,4 @@
 <?php define('LOGO', 'sport<i class="bi bi-app"></i>app') ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,8 +6,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?= isset($page_title) ? " | $page_title" : ""; ?></title>
-
     <title>sport app <?= isset($page_title) ? "| $page_title" : ""; ?></title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
@@ -18,10 +14,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 
-<body>
+<body class="d-flex min-vh-100">
     <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Expand at md</a>
+            <a class="navbar-brand" href="./"><?= LOGO ?></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -29,13 +25,21 @@
             <div class="collapse navbar-collapse" id="navbarsExample04">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="./about.php">about</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="./blog.php">blog</a>
                     </li>
 
+                    <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./sign_in">sign in</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="sign_up">sign up</a>
+                        </li>
 
+                    </ul>
                 </ul>
             </div>
         </div>
